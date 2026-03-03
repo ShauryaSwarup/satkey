98bf2365249ab6b2a11e59a4e26f5e334a783e97
Shaurya Swarup <srswarup_b21@ce.vjti.ac.in>
2026-03-03 23:55:45 +0530

    fix(Connect): update button styling for improved visual consistency

--- apps/frontend/components/Wallet/Connect.tsx
+++ apps/frontend/components/Wallet/Connect.tsx
@@ -69,7 +69,7 @@ const Connect = ({ onConnect }: ConnectProps) => {
   return (
     <button 
       onClick={onClick} 
-      className="p-2 group relative inline-flex items-center bg-white/10 backdrop-blur-md text-white rounded-full font-light tracking-wide overflow-hidden transition-all duration-300 hover:bg-white/20 hover:scale-105 active:scale-95 text-sm md:text-base hover:cursor-pointer"
+      className="p-2.5 border-white/10 border-[1px] group relative inline-flex items-center bg-white/10 backdrop-blur-md text-white rounded-full font-light tracking-wide overflow-hidden transition-all duration-300 hover:bg-white/20 hover:scale-105 active:scale-95 text-sm md:text-base hover:cursor-pointer"
     >
       <span className="relative z-10 text-orange-400 mx-1">Connect Wallet</span>
       <Key className="w-4 h-4 relative z-10 group-hover:translate-x-0.5 transition-transform" />
