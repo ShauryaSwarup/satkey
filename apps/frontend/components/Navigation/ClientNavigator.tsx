c1427b7aa2b458f366079106c93225524b9e50a7
Shaurya Swarup <srswarup_b21@ce.vjti.ac.in>
2026-03-10 17:47:40 +0530

    feat(hero): optimize hero section video

--- apps/frontend/components/Navigation/ClientNavigator.tsx
+++ apps/frontend/components/Navigation/ClientNavigator.tsx
@@ -1,17 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import Navigation from "@/components/Navigation/Navigation";
 
 const ClientNavigator = () => {
-    const [state, setState] = useState(false);
-
-    useEffect(() => {
-        setState(true);
-    }, []);
-
-    return ( state && <Navigation />);
-
+    return <Navigation />;
 };
 
 export default ClientNavigator;
